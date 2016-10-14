d0ed34fa398ba6d90fb57ae2acd45111709f447f
Sam Pearson <sam@xofz.com>
2016-10-13 21:15:13 -0700

    Add Likening

---
+++ xofz.Core/Framework/Erudition/Likening.cs
@@ -0,0 +1,35 @@
+﻿namespace xofz.Framework.Erudition
+{
+    using System;
+    using System.Linq;
+    using Materialization;
+
+    public class Likening
+    {
+        public virtual T Liken<T>(
+            Func<T> factory, 
+            MaterializedEnumerable<Action<T>> acts,
+            long limiter)
+        {
+            var actee = factory();
+            acts.First()(actee);
+            var firstHashCode = actee.GetHashCode();
+            foreach (var act in acts.Skip(1))
+            {
+                act(actee);
+                if (Math.Abs(actee.GetHashCode() - firstHashCode) <= limiter)
+                {
+                    continue;
+                }
+
+                return Liken(
+                    factory,
+                    new LinkedListMaterializedEnumerable<Action<T>>(
+                        acts.Except(new[] { act })),
+                        limiter);
+            }
+
+            return actee;
+        }
+    }
+}
