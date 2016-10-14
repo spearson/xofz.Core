63857b7e36aa424574711ff0d10388053c24f8cd
Sam Pearson <sam@xofz.com>
2016-10-13 20:29:31 -0700

    Add methods to Evaluator and Random

--- xofz.Core/Framework/Conditionality/Evaluator.cs
+++ xofz.Core/Framework/Conditionality/Evaluator.cs
@@ -6,7 +6,9 @@
 
     public class Evaluator
     {
-        public virtual bool Evaluate(IEnumerable<Func<bool>> conditionGenerators, BigInteger numberToCheck)
+        public virtual bool Evaluate(
+            IEnumerable<Func<bool>> conditionGenerators, 
+            BigInteger numberToCheck)
         {
             var enumerator = conditionGenerators.GetEnumerator();
             bool evaluation;
@@ -15,6 +17,29 @@
             {
                 enumerator.MoveNext();
                 evaluation = enumerator.Current();
+                if (!evaluation)
+                {
+                    return false;
+                }
+
+                ++counter;
+            }
+
+            return true;
+        }
+
+        public virtual bool Evaluate<T>(
+            IEnumerable<Func<T, bool>> conditions, 
+            T actor, 
+            BigInteger numberToCheck)
+        {
+            var enumerator = conditions.GetEnumerator();
+            bool evaluation;
+            BigInteger counter = 0;
+            while (counter < numberToCheck)
+            {
+                enumerator.MoveNext();
+                evaluation = enumerator.Current(actor);
                 if (!evaluation)
                 {
                     return false;
--- xofz.Core/Random.cs
+++ xofz.Core/Random.cs
@@ -1,6 +1,7 @@
 ﻿namespace xofz
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Numerics;
 
@@ -16,6 +17,18 @@
         {
             // i'll do something better than a shave to an int later
             return this.collection.Skip(this.random.Next((int)maxToPass)).FirstOrDefault();
+        }
+
+        public virtual IEnumerable<T> NextSequence(BigInteger maxToPass, BigInteger sourceSize)
+        {
+            var c = this.collection;
+            BigInteger counter = 0;
+            while (counter < sourceSize)
+            {
+                var nextSkip = this.random.Next((int)maxToPass);
+                ++counter;
+                yield return c.Skip(nextSkip).FirstOrDefault();
+            }
         }
 
         private readonly MaterializedEnumerable<T> collection;
