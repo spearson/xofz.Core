e0d0a63cc0be89b07b542952ae2c2fbab51290f5
Sam Pearson <sam@xofz.com>
2016-10-23 20:30:56 -0700

    Add Darkener

---
+++ xofz.Core/Framework/Erudition/Darkener.cs
@@ -0,0 +1,34 @@
+﻿namespace xofz.Framework.Erudition
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using Materialization;
+
+    public class Darkener
+    {
+        public virtual MaterializedEnumerable<T> Darken<T>(
+            IEnumerable<T> source, 
+            BigInteger limiter)
+        {
+            var hashCodes = new LinkedList<int>(); // hash codes should be longs
+            var darkenedCollection = new LinkedList<T>();
+
+            foreach (var item in source)
+            {
+                var hashCode = item.GetHashCode();
+                if (hashCodes.Any(hc => Math.Abs(hashCode - hc) < limiter))
+                {
+                    continue;
+                }
+
+                hashCodes.AddLast(hashCode);
+                darkenedCollection.AddLast(item);
+            }
+
+            return new LinkedListMaterializedEnumerable<T>(
+                darkenedCollection);
+        }
+    }
+}
