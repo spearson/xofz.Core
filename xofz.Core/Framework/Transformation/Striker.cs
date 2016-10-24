97ac7ce7a5ed25405815a7a3190e6701d8e254a9
Sam Pearson <sam@xofz.com>
2016-10-23 20:16:13 -0700

    Add Striker

---
+++ xofz.Core/Framework/Transformation/Striker.cs
@@ -0,0 +1,32 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System;
+
+    public class Striker<T, Y>
+    {
+        public Striker(
+            Translator<T, Y> translator,
+            Func<T> tFactory)
+        {
+            this.translator = translator;
+            this.tFactory = tFactory;
+        }
+
+        public Y Strike(
+            Action<T> tAction, 
+            Action<T, Y> transform,
+            Action<Y> yAction)
+        {
+            var t = this.tFactory();
+            tAction(t);
+
+            var y = this.translator.Translate(t, transform);
+            yAction(y);
+
+            return y;
+        }
+
+        private readonly Translator<T, Y> translator;
+        private readonly Func<T> tFactory;
+    }
+}
