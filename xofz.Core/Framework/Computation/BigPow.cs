6d172f666dc2767558e65b8f38dd3600a88a865c
Sam Pearson <sam@xofz.com>
2016-10-26 20:20:10 -0700

    Add BigPow

---
+++ xofz.Core/Framework/Computation/BigPow.cs
@@ -0,0 +1,18 @@
+﻿namespace xofz.Framework.Computation
+{
+    using System.Numerics;
+
+    public class BigPow
+    {
+        public virtual BigInteger Compute(BigInteger n, BigInteger exponent)
+        {
+            BigInteger result = 1;
+            for (BigInteger i = 0; i < exponent; ++i)
+            {
+                result *= n;
+            }
+
+            return result;
+        }
+    }
+}
