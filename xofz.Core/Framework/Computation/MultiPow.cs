a16dc443bf59275943a60169f020e55d1bcfd800
Sam Pearson <sam@xofz.com>
2016-10-29 18:40:46 -0700

    Add an overload to MultiPow.Compute()

--- xofz.Core/Framework/Computation/MultiPow.cs
+++ xofz.Core/Framework/Computation/MultiPow.cs
@@ -1,5 +1,7 @@
 ﻿namespace xofz.Framework.Computation
 {
+    using System;
+    using System.Collections.Generic;
     using System.Numerics;
 
     public class MultiPow
@@ -17,6 +19,20 @@
             {
                 result = bp.Compute(powers[i - 1], powers[i]);
                 powers[i - 1] = result;
+            }
+
+            return result;
+        }
+
+        public virtual BigInteger Compute(IEnumerable<BigInteger> powers)
+        {
+            var bp = this.bigPow;
+            var l = new List<BigInteger>(powers);
+            BigInteger result = 1;
+            for (var i = l.Count - 1; i > 0; --i)
+            {
+                result = bp.Compute(l[i - 1], l[i]);
+                l[i - 1] = result;
             }
 
             return result;
