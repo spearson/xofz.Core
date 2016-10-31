db8da6dbb48926d597e7950f6f66d54cdebc15f8
Sam Pearson <sam@xofz.com>
2016-10-30 20:33:24 -0700

    Add Innovator

---
+++ xofz.Core/Framework/Transformation/Innovator.cs
@@ -0,0 +1,48 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+
+    public class Innovator
+    {
+        public virtual IEnumerable<T> Innovate<T>(
+            MaterializedEnumerable<T>[] collections,
+            Func<T, BigInteger> valueChooser)
+        {
+            var enumerators = new LinkedList<IEnumerator<T>>();
+            for (var i = 0; i < collections.Length; ++i)
+            {
+                enumerators.AddLast(collections[i].GetEnumerator());
+            }
+
+            while (enumerators.First.Value.MoveNext())
+            {
+                BigInteger leastValue = long.MaxValue;
+                BigInteger maximalValue = 0;
+                var leastItem = default(T);
+                var maximalItem = default(T);
+                foreach (var enumerator in enumerators.Skip(1))
+                {
+                    enumerator.MoveNext();
+                    var value = valueChooser(enumerator.Current);
+                    if (value < leastValue)
+                    {
+                        leastValue = value;
+                        leastItem = enumerator.Current;
+                    }
+
+                    if (value > maximalValue)
+                    {
+                        maximalValue = value;
+                        maximalItem = enumerator.Current;
+                    }
+                }
+
+                yield return leastItem;
+                yield return maximalItem;
+            }
+        }
+    }
+}
