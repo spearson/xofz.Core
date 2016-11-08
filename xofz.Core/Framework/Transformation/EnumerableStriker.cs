f12eb4be484da07e21b7840395a0e28b39e4ee16
Sam Pearson <sam@xofz.com>
2016-11-07 23:34:52 -0800

    Add EnumerableStriker

---
+++ xofz.Core/Framework/Transformation/EnumerableStriker.cs
@@ -0,0 +1,34 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EnumerableStriker
+    {
+        public virtual IEnumerable<T> Strike<T, K>(
+            IEnumerable<T> tSource,
+            IEnumerable<K> kSource,
+            Func<T, K, T> strike)
+        {
+            var te = tSource.GetEnumerator();
+            var ke = kSource.GetEnumerator();
+            while (te.MoveNext() || ke.MoveNext())
+            {
+                yield return strike(te.Current, ke.Current);
+            }
+        }
+
+        public virtual IEnumerable<K> Strike<T, K>(
+            IEnumerable<T> tSource,
+            IEnumerable<K> kSource,
+            Func<T, K, K> strike)
+        {
+            var te = tSource.GetEnumerator();
+            var ke = kSource.GetEnumerator();
+            while (te.MoveNext() || ke.MoveNext())
+            {
+                yield return strike(te.Current, ke.Current);
+            }
+        }
+    }
+}
