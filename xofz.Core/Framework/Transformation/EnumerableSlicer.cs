27348a97261ebdbb18bee18e3cb3eeaa6ea1f76c
Sam Pearson <sam@xofz.com>
2016-11-07 22:58:44 -0800

    Add EnumerableSlicer

---
+++ xofz.Core/Framework/Transformation/EnumerableSlicer.cs
@@ -0,0 +1,28 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using xofz.Framework.Materialization;
+
+    public class EnumerableSlicer
+    {
+        public virtual MaterializedEnumerable<T>[] Slice<T>(
+            IEnumerable<T> source,
+            MaterializedEnumerable<int> slicePoints)
+        {
+            var ll = new LinkedList<T>(source);
+            var array = new MaterializedEnumerable<T>[slicePoints.Count];
+            var counter = 0;
+            foreach (var slicePoint in slicePoints)
+            {
+                var sequence = ll.Take(slicePoint);
+                array[counter] =
+                    new LinkedListMaterializedEnumerable<T>(sequence);
+                ++counter;
+                ll = new LinkedList<T>(ll.Skip(slicePoint));
+            }
+
+            return array;
+        }
+    }
+}
