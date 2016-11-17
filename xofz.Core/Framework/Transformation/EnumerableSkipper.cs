6313961355c02423df6c828f3924fda132bfe95c
Sam Pearson <sam@xofz.com>
2016-11-16 22:08:06 -0800

    Add EnumerableSkipper

---
+++ xofz.Core/Framework/Transformation/EnumerableSkipper.cs
@@ -0,0 +1,37 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+    using xofz.Framework.Materialization;
+
+    public class EnumerableSkipper
+    {
+        public virtual IEnumerable<T> Skip<T>(IEnumerable<T> source, int skipPoint)
+        {
+            var enumerator = source.GetEnumerator();
+            var counter = 0;
+            while (enumerator.MoveNext())
+            {
+                if (counter == skipPoint)
+                {
+                    counter = 0;
+                    yield return enumerator.Current;
+                }
+
+                ++counter;
+            }
+        }
+
+        public virtual MaterializedEnumerable<T> SkipThrough<T>(IEnumerable<T> finiteSource, int skipPoint)
+        {
+            var ll = new LinkedList<T>(finiteSource);
+            var result = new List<T>();
+            for (var i = 0; i < skipPoint; ++i)
+            {
+                result.AddRange(this.Skip(ll, skipPoint));
+                ll.RemoveFirst();
+            }
+
+            return new OrderedMaterializedEnumerable<T>(result);
+        }
+    }
+}
