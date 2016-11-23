e95032ffeeb9e70d8d85ce8f98122032eb9dc116
Sam Pearson <sam@xofz.com>
2016-11-22 23:54:08 -0800

    Add EnumerablePicker

---
+++ xofz.Core/Framework/Transformation/EnumerablePicker.cs
@@ -0,0 +1,26 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+    using System.Numerics;
+
+    public class EnumerablePicker
+    {
+        public virtual IEnumerable<T> Pick<T>(
+            IEnumerable<T> source,
+            MaterializedEnumerable<BigInteger> pickPoints)
+        {
+            BigInteger counter = 0;
+            var enumerator = source.GetEnumerator();
+            foreach (var pickPoint in pickPoints)
+            {
+                while (counter < pickPoint)
+                {
+                    enumerator.MoveNext();
+                    ++counter;
+                }
+
+                yield return enumerator.Current;
+            }
+        }
+    }
+}
