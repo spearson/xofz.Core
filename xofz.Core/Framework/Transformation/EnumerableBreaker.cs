48ff066e539d980f81553f3df6f22a54a39323f3
Sam Pearson <sam@xofz.com>
2016-12-08 19:35:56 -0800

    Add EnumerableBreaker

---
+++ xofz.Core/Framework/Transformation/EnumerableBreaker.cs
@@ -0,0 +1,43 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EnumerableBreaker
+    {
+        public virtual IEnumerable<T> AddBreak<T>(
+            IEnumerable<T> source, 
+            Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
+
+        public virtual IEnumerable<T> AddBreak<T>(
+            IEnumerable<T> source, 
+            params Func<T, bool>[] predicates)
+        {
+            foreach (var item in source)
+            {
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(item))
+                    {
+                        yield break;
+                    }
+                }
+
+                yield return item;
+            }
+        }
+    }
+}
