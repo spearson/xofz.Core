6889b296e6f714818c1219854444a7bee62ed00b
Sam Pearson <sam@xofz.com>
2016-12-08 18:57:25 -0800

    Add EnumerableSpreader

---
+++ xofz.Core/Framework/Transformation/EnumerableSpreader.cs
@@ -0,0 +1,22 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+
+    public class EnumerableSpreader
+    {
+        public virtual IEnumerable<T> Spread<T>(IEnumerable<T> source, int spread)
+        {
+            var counter = 0;
+            foreach (var item in source)
+            {
+                while (counter < spread)
+                {
+                    yield return item;
+                    ++counter;
+                }
+
+                counter = 0;
+            }
+        }
+    }
+}
