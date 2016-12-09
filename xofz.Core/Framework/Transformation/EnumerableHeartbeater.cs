bc7277688a7e39e4e343477fd0f4b3bb876dd687
Sam Pearson <sam@xofz.com>
2016-12-08 19:05:40 -0800

    Add EnumerableHeartbeater

---
+++ xofz.Core/Framework/Transformation/EnumerableHeartbeater.cs
@@ -0,0 +1,22 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+
+    public class EnumerableHeartbeater
+    {
+        public virtual IEnumerable<T> AddHeartbeat<T>(IEnumerable<T> source, T heartbeat, int interval)
+        {
+            var counter = 0;
+            foreach (var item in source)
+            {
+                yield return item;
+                ++counter;
+                if (counter == interval)
+                {
+                    counter = 0;
+                    yield return heartbeat;
+                }
+            }
+        }
+    }
+}
