60bd5e1bf7a44df6bbb7c4de765610760e6be992
Sam Pearson <sam@xofz.com>
2016-12-09 14:29:16 -0800

    Add EnumerableInjector

---
+++ xofz.Core/Framework/Transformation/EnumerableInjector.cs
@@ -0,0 +1,28 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class EnumerableInjector
+    {
+        public virtual IEnumerable<T> Inject<T>(
+            IEnumerable<T> source,
+            MaterializedEnumerable<int> injectionPoints,
+            params T[] injections)
+        {
+            var counter = 0;
+            var index = 0;
+            foreach (var item in source)
+            {
+                ++counter;
+                if (injectionPoints.Contains(counter))
+                {
+                    yield return injections[index];
+                    ++index;
+                }
+
+                yield return item;
+            }
+        }
+    }
+}
