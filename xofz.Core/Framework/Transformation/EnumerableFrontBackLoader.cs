c82ac62546055e369ab1678029b3d73d145ab970
Sam Pearson <sam@xofz.com>
2016-12-08 19:26:14 -0800

    Add EnumerableFrontBackLoader

---
+++ xofz.Core/Framework/Transformation/EnumerableFrontBackLoader.cs
@@ -0,0 +1,33 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+
+    public class EnumerableFrontBackLoader
+    {
+        public IEnumerable<T> FrontLoad<T>(IEnumerable<T> source, params T[] frontItems)
+        {
+            foreach (var frontItem in frontItems)
+            {
+                yield return frontItem;
+            }
+
+            foreach (var item in source)
+            {
+                yield return item;
+            }
+        }
+
+        public IEnumerable<T> BackLoad<T>(IEnumerable<T> source, params T[] backItems)
+        {
+            foreach (var item in source)
+            {
+                yield return item;
+            }
+
+            foreach (var backItem in backItems)
+            {
+                yield return backItem;
+            }
+        }
+    }
+}
