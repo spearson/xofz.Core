4f1b8c2fe63ed67a7704f5fd78429bfb0a878f8c
Sam Pearson <sam@xofz.com>
2016-12-10 12:16:08 -0800

    Add EnumerableSelector

---
+++ xofz.Core/Framework/Transformation/EnumerableSelector.cs
@@ -0,0 +1,21 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+
+    public class EnumerableSelector
+    {
+        public virtual IEnumerable<T> Select<T>(IEnumerable<T> source, IEnumerable<bool> selectors)
+        {
+            var e1 = source.GetEnumerator();
+            var e2 = selectors.GetEnumerator();
+            while (e1.MoveNext())
+            {
+                e2.MoveNext();
+                if (e2.Current)
+                {
+                    yield return e1.Current;
+                }
+            }
+        }
+    }
+}
