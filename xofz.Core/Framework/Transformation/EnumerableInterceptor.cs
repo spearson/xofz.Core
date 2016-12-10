69ccc33ee2864409cf39c1c49724e8a76ad78736
Sam Pearson <sam@xofz.com>
2016-12-10 11:47:53 -0800

    Add EnumerableInterceptor

---
+++ xofz.Core/Framework/Transformation/EnumerableInterceptor.cs
@@ -0,0 +1,46 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+
+    public class EnumerableInterceptor
+    {
+        public virtual IEnumerable<T> Intercept<T>(
+            IEnumerable<T> source, 
+            T interception, 
+            int interceptionPoint)
+        {
+            var counter = 0;
+            foreach (var item in source)
+            {
+                yield return item;
+                ++counter;
+                if (counter == interceptionPoint)
+                {
+                    yield return interception;
+                }
+            }
+        }
+
+        public virtual IEnumerable<T> Intercept<T>(
+            IEnumerable<T> source,
+            MaterializedEnumerable<T> interception,
+            int interceptionPoint)
+        {
+            var counter = 0;
+            foreach (var item in source)
+            {
+                yield return item;
+                ++counter;
+                if (counter != interceptionPoint)
+                {
+                    continue;
+                }
+
+                foreach (var intercept in interception)
+                {
+                    yield return intercept;
+                }
+            }
+        }
+    }
+}
