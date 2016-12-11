6c72c2a8428bbb0b14929941eb48b0ae3a169d8a
Sam Pearson <sam@xofz.com>
2016-12-10 21:05:55 -0800

    Add EnumerableDisperser and EnumerableDragger

---
+++ xofz.Core/Framework/Transformation/EnumerableDragger.cs
@@ -0,0 +1,27 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+
+    public class EnumerableDragger
+    {
+        public virtual IEnumerable<T> Drag<T>(
+            IEnumerable<T> source,
+            MaterializedEnumerable<int> dragLengths)
+        {
+            var e = source.GetEnumerator();
+            foreach (var length in dragLengths)
+            {
+                e.MoveNext();
+                for (var i = 0; i < length; ++i)
+                {
+                    yield return e.Current;
+                }
+            }
+
+            while (e.MoveNext())
+            {
+                yield return e.Current;
+            }
+        }
+    }
+}
