230a7794fdfc1a0e9b266d3842dbee3b2f167613
Sam Pearson <sam@xofz.com>
2016-12-11 20:23:56 -0800

    Add EnumerableRaker

---
+++ xofz.Core/Framework/Transformation/EnumerableRaker.cs
@@ -0,0 +1,31 @@
+﻿namespace xofz.Framework.Transformation
+{
+    using System.Collections.Generic;
+
+    public class EnumerableRaker
+    {
+        public virtual IEnumerable<T> Rake<T>(
+            IEnumerable<T> source,
+            IEnumerable<int> passPoints)
+        {
+            var e = source.GetEnumerator();
+            var counter = 0;
+            foreach (var passPoint in passPoints)
+            {
+                while (counter < passPoint)
+                {
+                    e.MoveNext();
+                    ++counter;
+                }
+
+                yield return e.Current;
+                counter = 0;
+            }
+
+            while (e.MoveNext())
+            {
+                yield return e.Current;
+            }
+        }
+    }
+}
