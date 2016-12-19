a5c37421b9475bb0446207626c1a9b42617ea563
Sam Pearson <sam@xofz.com>
2016-12-18 23:58:06 -0800

    Add MethodWeb

---
+++ xofz.Core/Framework/MethodWeb.cs
@@ -0,0 +1,46 @@
+﻿namespace xofz.Framework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MethodWeb
+    {
+        public MethodWeb()
+        {
+            this.dependencies = new List<object>();
+        }
+
+        public virtual void RegisterDependency(object dependency)
+        {
+            this.dependencies.Add(dependency);
+        }
+
+        public virtual U Run<T, U>(Func<T, U> method)
+        {
+            var dependency = this.dependencies.FirstOrDefault(d => d is T);
+            if (dependency == null)
+            {
+                return default(U);
+            }
+
+            return method((T)dependency);
+        }
+
+        public virtual T Run<T>(Action<T> method)
+        {
+            var dependency = this.dependencies.FirstOrDefault(d => d is T);
+            if (dependency == null)
+            {
+                return default(T);
+            }
+
+            var t = (T)dependency;
+            method(t);
+
+            return t;
+        }
+
+        private readonly List<object> dependencies;
+    }
+}
