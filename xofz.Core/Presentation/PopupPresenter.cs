2803b8436990723206d5664f9b7bf5d84921c225
Sam Pearson <sam@xofz.com>
2017-04-08 20:52:51 -0700

    Add PopupPresenter.  This is a base presenter class that Displays() and Hides() a popup UI when Start() and Stop() are called

---
+++ xofz.Core/Presentation/PopupPresenter.cs
@@ -0,0 +1,28 @@
+﻿namespace xofz.Presentation
+{
+    using xofz.UI;
+
+    public class PopupPresenter : Presenter
+    {
+        public PopupPresenter(
+            PopupUi ui) 
+            : base(ui, null)
+        {
+            this.ui = ui;
+        }
+
+        public override void Start()
+        {
+            UiHelpers.Write(this.ui, () => this.ui.Display());
+            this.ui.WriteFinished.WaitOne();
+        }
+
+        public override void Stop()
+        {
+            UiHelpers.Write(this.ui, () => this.ui.Hide());
+            this.ui.WriteFinished.WaitOne();
+        }
+
+        private readonly PopupUi ui;
+    }
+}
