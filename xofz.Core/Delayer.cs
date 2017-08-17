2db8c8bad383056f18593774e047fe189437b842
Sam Pearson <sam@xofz.com>
2017-08-16 23:20:02 -0700

    Add Delayer

---
+++ xofz.Core/Delayer.cs
@@ -0,0 +1,42 @@
+﻿namespace xofz
+{
+    using System.Threading;
+
+    public class Delayer
+    {
+        public Delayer()
+            : this(ThreadPriority.AboveNormal)
+        {
+        }
+
+        public Delayer(ThreadPriority priority)
+        {
+            this.priority = priority;
+            this.finishedSleeping = new ManualResetEvent(true);
+        }
+
+        // please have one delayer per thread
+        public virtual void Delay(int milliseconds)
+        {
+            var fs = this.finishedSleeping;
+            fs.Reset();
+            ThreadPool.QueueUserWorkItem(
+                state =>
+                {
+                    Thread.Sleep(milliseconds);
+                    fs.Set();
+                });
+            var t = new Thread(() =>
+                {
+                    Thread.Sleep(milliseconds);
+                    fs.Set();
+                })
+                { Priority = this.priority };
+            t.Start();
+            fs.WaitOne(milliseconds);
+        }
+
+        private readonly ThreadPriority priority;
+        private readonly ManualResetEvent finishedSleeping;
+    }
+}
