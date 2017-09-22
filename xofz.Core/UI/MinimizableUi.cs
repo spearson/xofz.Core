f22d82698683e39a0cba4644d0a1849c0297b5f4
Sam Pearson <sam@xofz.com>
2017-09-22 11:05:19 -0700

    MinimizableUi: update with Restore() method and Minimized/Restored events

--- xofz.Core/UI/MinimizableUi.cs
+++ xofz.Core/UI/MinimizableUi.cs
@@ -1,7 +1,15 @@
 ﻿namespace xofz.UI
 {
+    using System;
+
     public interface MinimizableUi : Ui
     {
+        event Action Minimized;
+
+        event Action Restored;
+        
         void Minimize();
+
+        void Restore();
     }
 }
