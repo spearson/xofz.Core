7e27e87b5d1dd841df347da21edd464b2c4f47fe
Sam Pearson <sam@xofz.com>
2017-09-26 11:44:43 -0700

    Add AbPlc, a factory, and an implementation

---
+++ xofz.Core/Framework/Plc/AbPlc.cs
@@ -0,0 +1,9 @@
+﻿namespace xofz.Framework.Plc
+{
+    public interface AbPlc
+    {
+        string Read(string address);
+
+        void Write(string address, string value);
+    }
+}
---
+++ xofz.Core/Framework/Plc/Implementation/EthernetAbPlc.cs
@@ -0,0 +1,37 @@
+﻿namespace xofz.Framework.Plc.Implementation
+{
+    using System;
+    using AdvancedHMIDrivers;
+    using xofz.Internal;
+
+    internal sealed class EthernetAbPlc : AbPlc
+    {
+        static EthernetAbPlc()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve
+                -= EmbeddedAssemblyLoader.Load;
+            AppDomain.CurrentDomain.AssemblyResolve
+                += EmbeddedAssemblyLoader.Load;
+        }
+
+        public EthernetAbPlc(string ipAddress)
+        {
+            this.driver = new EthernetIPforSLCMicroCom
+            {
+                IPAddress = ipAddress
+            };
+        }
+
+        string AbPlc.Read(string address)
+        {
+            return this.driver.Read(address);
+        }
+
+        void AbPlc.Write(string address, string value)
+        {
+            this.driver.Write(address, value);
+        }
+
+        private readonly EthernetIPforSLCMicroCom driver;
+    }
+}
