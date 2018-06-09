7e454d5ab1998081fcdd16c6484952ae5c6c2656
Sam Pearson <sam@xofz.com>
2018-06-08 17:36:34 -0700

    EventSubscriber: Add more Subscribe methods to help with subscribing to member fields.

--- xofz.Core/Framework/EventSubscriber.cs
+++ xofz.Core/Framework/EventSubscriber.cs
@@ -2,11 +2,65 @@
 {
     using System;
 
+    // this class is pretty much for testing that event subscriptions went through
+    // because += is hard to test
     public class EventSubscriber
     {
         public EventSubscriber(MethodWeb web)
         {
             this.web = web;
+        }
+
+        public virtual void Subscribe<T>(
+            T publisher,
+            string eventName,
+            Action handler)
+        {
+            publisher
+                .GetType()
+                .GetEvent(eventName)
+                ?.AddEventHandler(
+                    publisher,
+                    handler);
+        }
+
+        public virtual void Subscribe<T, U>(
+            T publisher,
+            string eventName,
+            Action<U> handler)
+        {
+            publisher
+                .GetType()
+                .GetEvent(eventName)
+                ?.AddEventHandler(
+                    publisher,
+                    handler);
+        }
+
+        public virtual void Subscribe<T, U, V>(
+            T publisher,
+            string eventName,
+            Action<U, V> handler)
+        {
+            publisher
+                .GetType()
+                .GetEvent(eventName)
+                ?.AddEventHandler(
+                    publisher,
+                    handler);
+        }
+
+        public virtual void Subscribe<T, U, V, W>(
+            T publisher,
+            string eventName,
+            Action<U, V, W> handler)
+        {
+            publisher
+                .GetType()
+                .GetEvent(eventName)
+                ?.AddEventHandler(
+                    publisher,
+                    handler);
         }
 
         public virtual void Subscribe<T>(
