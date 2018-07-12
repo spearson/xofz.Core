de3f8f74170150c89875382a1b3d24e704059f94
Sam Pearson <sam@xofz.com>
2018-07-11 19:58:55 -0700

    Found a way to get VersionReader to work from within xofz.Core; also add an overload that reads the version of xofz.Core

---
+++ xofz.Core/Framework/VersionReader.cs
@@ -0,0 +1,56 @@
+﻿namespace xofz.Framework
+{
+    using System;
+    using System.Reflection;
+    using System.Text;
+
+    public class VersionReader
+    {
+        public VersionReader()
+            : this(Assembly.GetEntryAssembly())
+        {
+        }
+
+        public VersionReader(
+            Assembly executingAssembly)
+        {
+            if (executingAssembly == null)
+            {
+                throw new ArgumentNullException(
+                    nameof(executingAssembly));
+            }
+
+            this.executingAssembly = executingAssembly;
+        }
+
+        public virtual string Read()
+        {
+            var ea = this.executingAssembly;
+            return this.readInternal(ea);
+        }
+        
+        public virtual string ReadCoreVersion()
+        {
+            var ea = Assembly.GetExecutingAssembly();
+            return this.readInternal(ea);
+        }
+
+        private string readInternal(Assembly assembly)
+        {
+            var an = new AssemblyName(assembly.FullName);
+            var v = an.Version;
+            var versionBuilder = new StringBuilder();
+            versionBuilder.Append(v.Major);
+            versionBuilder.Append('.');
+            versionBuilder.Append(v.Minor);
+            versionBuilder.Append('.');
+            versionBuilder.Append(v.Build);
+            versionBuilder.Append('.');
+            versionBuilder.Append(v.MinorRevision);
+
+            return versionBuilder.ToString();
+        }
+
+        private readonly Assembly executingAssembly;
+    }
+}
