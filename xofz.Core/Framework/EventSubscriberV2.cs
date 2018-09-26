806ffbf1416b6548b4eab9a119dd9e498f1271d1
Sam Pearson <sam@xofz.com>
2018-09-26 14:00:45 -0700

    Add EventSubscriberV2 for handling System.Action delegates

---
+++ xofz.Core/Framework/EventSubscriberV2.cs
@@ -0,0 +1,60 @@
+﻿namespace xofz.Framework
+{
+    using System;
+
+    public class EventSubscriberV2 
+        : EventSubscriber
+    {
+        public virtual void Subscribe(
+            object publisher,
+            string eventName,
+            Action handler)
+        {
+            publisher
+                ?.GetType()
+                .GetEvent(eventName)
+                ?.AddEventHandler(
+                    publisher,
+                    handler);
+        }
+
+        public virtual void Subscribe<T>(
+            object publisher,
+            string eventName,
+            Action<T> handler)
+        {
+            publisher
+                ?.GetType()
+                .GetEvent(eventName)
+                ?.AddEventHandler(
+                    publisher,
+                    handler);
+        }
+
+        public virtual void Subscribe<T, U>(
+            object publisher,
+            string eventName,
+            Action<T, U> handler)
+        {
+            publisher
+                ?.GetType()
+                .GetEvent(eventName)
+                ?.AddEventHandler(
+                    publisher,
+                    handler);
+        }
+
+        public virtual void Subscribe<T, U, V>(
+            object publisher,
+            string eventName,
+            Action<T, U, V> handler)
+        {
+            publisher
+                ?.GetType()
+                .GetEvent(eventName)
+                ?.AddEventHandler(
+                    publisher,
+                    handler);
+        }
+    }
+}
