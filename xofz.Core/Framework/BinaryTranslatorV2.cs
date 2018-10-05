077959a6c39da8e1c00402b152f6716f254cc069
Sam Pearson <sam@xofz.com>
2018-10-05 13:27:59 -0700

    Add BinaryTranslatorV2, which allows for getting bits from BigIntegers

---
+++ xofz.Core/Framework/BinaryTranslatorV2.cs
@@ -0,0 +1,24 @@
+﻿namespace xofz.Framework
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+
+    public class BinaryTranslatorV2
+        : BinaryTranslator
+    {
+        public virtual BigInteger ReadBigInteger(
+            IEnumerable<bool> bits)
+        {
+            var bytes = this.GetBytes(bits);
+            return new BigInteger(bytes?.ToArray() ?? new byte[0]);
+        }
+
+        public virtual IEnumerable<bool> GetBits(
+            BigInteger number)
+        {
+            return this.GetBits(
+                number.ToByteArray());
+        }
+    }
+}
