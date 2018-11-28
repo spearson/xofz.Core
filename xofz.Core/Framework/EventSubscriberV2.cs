c15f687041da6985589fa0cd72a84b5f73037e93
sam <sam@xofz.com>
2018-11-27 23:36:01 -0800

    EventSubscriberV2: add unsubscribe methods

--- xofz.Core/Framework/EventSubscriberV2.cs
+++ xofz.Core/Framework/EventSubscriberV2.cs
@@ -56,5 +56,57 @@
                     publisher,
                     handler);
         }
+
+        public virtual void Unsubscribe(
+            object publisher,
+            string eventName,
+            Action handler)
+        {
+            publisher
+                ?.GetType()
+                .GetEvent(eventName)
+                ?.RemoveEventHandler(
+                    publisher,
+                    handler);
+        }
+
+        public virtual void Unsubscribe<T>(
+            object publisher,
+            string eventName,
+            Action<T> handler)
+        {
+            publisher
+                ?.GetType()
+                .GetEvent(eventName)
+                ?.RemoveEventHandler(
+                    publisher,
+                    handler);
+        }
+
+        public virtual void Unsubscribe<T, U>(
+            object publisher,
+            string eventName,
+            Action<T, U> handler)
+        {
+            publisher
+                ?.GetType()
+                .GetEvent(eventName)
+                ?.RemoveEventHandler(
+                    publisher,
+                    handler);
+        }
+
+        public virtual void Unsubscribe<T, U, V>(
+            object publisher,
+            string eventName,
+            Action<T, U, V> handler)
+        {
+            publisher
+                ?.GetType()
+                .GetEvent(eventName)
+                ?.RemoveEventHandler(
+                    publisher,
+                    handler);
+        }
     }
 }
