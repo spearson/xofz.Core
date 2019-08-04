fe4a7bc418e6834e864c84a6ee55068d4192bb00
sam <sam@xofz.com>
2019-08-04 12:56:08 -0700

    Add WpfUiMessagePumper

---
+++ xofz.Core/UI/WPF/WpfUiMessagePumper.cs
@@ -0,0 +1,17 @@
+﻿namespace xofz.UI.WPF
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Threading;
+
+    public sealed class WpfUiMessagePumper 
+        : UiMessagePumper
+    {
+        void UiMessagePumper.Pump()
+        {
+            Application.Current?.Dispatcher?.Invoke(
+                DispatcherPriority.Send,
+                (Action) (() => { }));
+        }
+    }
+}
